6b4f825d9ae90be85bc8d984174ee4c690fae64a
pavlo.shiba <pavlo.shiba@gmail.com>
2015-12-13 21:14:37 +0200

    update switch-operator

--- BotManager.cpp
+++ BotManager.cpp
@@ -1,5 +1,12 @@
 #include "BotManager.h"
 
+enum BOT_LEVEL
+{
+    BOT_LOW_LEVEL = 0,
+    BOT_MEDIUM_LEVEL,
+    BOT_HIGH_LEVEL
+};
+
 GameBot* BotManager::getBot(int level)
 {
     GameBot* bot;
@@ -7,13 +14,13 @@ GameBot* BotManager::getBot(int level)
     switch(level)
     {
     default:
-    case 0:
+    case BOT_LOW_LEVEL:
         bot = new LowBot;
         break;
-    case 1:
+    case BOT_MEDIUM_LEVEL:
         bot = new MediumBot;
         break;
-    case 2:
+    case BOT_HIGH_LEVEL:
         bot = new HardBot;
         break;
     };
